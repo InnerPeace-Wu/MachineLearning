36fcc80445b59d0313d7713e1a97888be5f570fe
InnerPeace-Wu <innerpeace.wu@outlook.com>
2016-11-30 20:47:01 +0800

    update project HWDR

---
+++ HWDR/lrpridect.m
@@ -0,0 +1,32 @@
+function p = lrpridect(X)
+
+if ~exist('all_theta', 'file') 
+    load lr_theta.mat;
+end
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+maxt = zeros(size(X, 1), 1);
+A = X * all_theta';
+[maxt,p] = max(A, [], 2);
+
+end
